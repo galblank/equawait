f1219b5cbf93940fc3690bc25e33c87d6ede43e1
Blank, Gal (Contractor) <gal_blank@comcast.com>
2016-11-04 16:19:08 -0400

    public

--- EquaWait.swift
+++ EquaWait.swift
@@ -100,7 +100,7 @@ public class EquaWait: UIView,barCycleDelegate {
         addSubview(textlabel)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
